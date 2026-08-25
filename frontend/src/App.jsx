14597db55d4f89a02d496bc30f586f6f08bdb0cf
Ankur Konan <ankurkonan@gmail.com>
2026-08-25 18:35:56 +0530

    Update finance tracker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
+        {/* <Route path="/register" element={<Register />} />*/}
         <Route
           path="/dashboard"
           element={
